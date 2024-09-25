0464dcfe9cc2f2de039ec5ad684b431e13c2b1b0
Badmatrix <matrix.og11@gmail.com>
2024-09-25 06:57:27 +0100

    updated some component classes

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -31,23 +31,47 @@ export default function Modal({
     });
   }
   return (
-    <div
-      className={`modal fixed right-0 top-0 z-50 h-screen w-full space-y-8 bg-neutral-800 py-10  md:w-1/3 xl:w-1/4 ${openModal ? "" : " "}`}
-    >
-      <div className="px-5">
-        <MdOutlineClose
-          className="cursor-pointer text-4xl transition-all duration-200 ease-in-out hover:text-slate-300"
-          onClick={handleToggle}
-        />
+    <section className="fixed left-0 top-0 z-20 h-full w-full overflow-hidden backdrop-blur-sm">
+      <div
+        className={`absolute right-0 top-0 h-screen w-0 transform overflow-x-hidden bg-neutral-800 transition-all duration-500 ease-linear ${openModal ? "w-full sm:w-1/2 md:w-1/3" : "w-0"}`}
+      >
+        <div className="modal space-y-8">
+          <div className="px-5 py-3">
+            <MdOutlineClose
+              className="cursor-pointer text-4xl transition-all duration-200 ease-in-out hover:text-slate-300"
+              onClick={handleToggle}
+            />
+          </div>
+          <Fade cascade direction="down" duration={500}>
+            <ul className="float-right space-y-8 px-5 text-right text-xl capitalize lg:px-10">
+              <li onClick={gotoAbout}>about me</li>
+              <li onClick={gotoProject}>projects</li>
+              <li onClick={gotoContact}>contact</li>
+              {
+                <li>
+                  <a href="#" target="_blank" rel="noopener noreferrer"></a>
+                </li>
+              }
+            </ul>
+          </Fade>
+        </div>
       </div>
-        <Fade cascade direction="down" duration={500}>
-      <ul className="float-right space-y-8 px-5 text-right text-xl capitalize lg:px-10">
-          <li onClick={gotoAbout}>about me</li>
-          <li onClick={gotoProject}>projects</li>
-          <li onClick={gotoContact}>contact</li>
-          {/* <li><a href="#" target="_blank" rel="noopener noreferrer"></a></li> */}
-      </ul>
-        </Fade>
-    </div>
+    </section>
+ 
+    //  <div>
+    //   <div>
+    //     <button onClick={() => setOpenPanel(true)}>Open</button>
+    //   </div>
+    //   <SlidingPanel
+    //     type={'left'}
+    //     isOpen={openPanel}
+    //     size={30}
+    //   >
+    //     <div>
+    //       <div>My Panel Content</div>
+    //       <button onClick={() => setOpenPanel(false)}>close</button>
+    //     </div>
+    //   </SlidingPanel>
+    // </div>
   );
 }
